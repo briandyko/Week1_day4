4ebbb92c5179571dbaceb542b7d7fc43b6cb1ad7
briandyko <briandyko@gmail.com>
2017-09-28 14:44:45 -0400

    further study of strings

--- Week1_Day4/Program.cs
+++ Week1_Day4/Program.cs
@@ -10,6 +10,28 @@ namespace Week1_Day4
     {
         static void Main(string[] args)
         {
+            //Console.WriteLine("This is a tab \t. ");
+            //Console.WriteLine("This is a \"quote\".");
+            //Console.WriteLine("This is a backslash \\");
+            //Console.WriteLine("This is a \n new line.");
+            //Console.WriteLine("And this is a bell \a");
+
+            string firstName = "Daniel";
+            string dogName = "Gus";
+
+            //Console.WriteLine("My name is {0} and my dog is {1}.", firstName, dogName);
+
+            //string bestFriends = firstName + " " + dogName;
+            //Console.WriteLine(bestFriends);
+
+           string bestFriends = string.Concat(firstName, " ", dogName);
+           Console.WriteLine(bestFriends);
+
+            Console.WriteLine(firstName.Length);
+            Console.WriteLine(bestFriends.Length);
+
+
+
         }
     }
 }
